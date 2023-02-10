49e520b9033da09011f07c1d921cc555a3aae562
Nwosu Uchenna Franklin <itsnwosuuche@gmail.com>
2023-02-10 22:02:31 +0100

    Application of Responsive Design to the project to fit mobile view

--- omnifood.js
+++ omnifood.js
@@ -1,36 +1,40 @@
 const learnMore = document.querySelector(".btn2");
 const omniFoodInfo = document.querySelector(".omnifood-info");
 const closeBtn = document.querySelector(".close-btn");
-// const blurScreen = document.querySelector(".blur");
-document.querySelector(".blur").style.display = "none";
+const blurScreen = document.querySelector(".blur");
+const menuIcon = document.querySelector(".ionicon");
+const navLinks = document.querySelector(".nav-links");
+
+var condition = false;
+menuIcon.addEventListener("click", () => {
+  if (!condition) {
+    navLinks.style.display = "flex";
+    navLinks.style.right = "0";
+    condition = true;
+  } else {
+    navLinks.style.right = "-30%";
+    condition = false;
+  }
+});
 
 learnMore.addEventListener("click", (e) => {
   omniFoodInfo.style.display = "block";
-  document.querySelector(".blur").style.display = "block";
-  //   blurScreen.style.display = "block";
+  blurScreen.style.display = "block";
 });
 
 closeBtn.addEventListener("click", (e) => {
   omniFoodInfo.style.display = "none";
-  document.querySelector(".blur").style.display = "none";
-  //   blurScreen.style.display = "none";
+  blurScreen.style.display = "none";
 });
 
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     omniFoodInfo.style.display = "none";
-    document.querySelector(".blur").style.display = "none";
-    // blurScreen.style.display = "none";
+    blurScreen.style.display = "none";
   }
 });
 
-document.querySelector(".blur").addEventListener("click", (e) => {
+blurScreen.addEventListener("click", (e) => {
   omniFoodInfo.style.display = "none";
-  document.querySelector(".blur").style.display = "none";
-  //    blurScreen.style.display = "none";
+  blurScreen.style.display = "none";
 });
-
-
-
-
-
